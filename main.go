5db313dc1616abe57c5ccc5fa0ffd88635c525af
Tamal Saha <tamal@appscode.com>
2022-07-20 19:20:45 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -18,11 +18,27 @@ func main() {
 		Namespace: "demo",
 		Name:      "es-quickstart-0",
 	}
+	/*
+		// k exec -it es-quickstart-0 -c elasticsearch -- curl -k https://localhost:9200
+		out, err := exec.Exec(
+			cfg,
+			pod,
+			exec.TTY(true),
+			exec.Container("elasticsearch"),
+			exec.Command("curl", "-k", "https://localhost:9200"),
+		)
+	*/
+	// k exec -it es-quickstart-0 -c elasticsearch -- bash -c 'curl -XGET -k -u "$ELASTIC_USER:$ELASTIC_PASSWORD" "https://localhost:9200/_cluster/health?pretty"'
 	out, err := exec.Exec(
 		cfg,
 		pod,
+		exec.TTY(true),
 		exec.Container("elasticsearch"),
+		// exec.Command("bash", "-c", `curl -k https://localhost:9200`),
 		exec.Command("bash", "-c", `curl -XGET -k -u "$ELASTIC_USER:$ELASTIC_PASSWORD" "https://localhost:9200/_cluster/health?pretty"`),
+
+		// exec.Command("curl -k https://localhost:9200"),
+		// exec.Command(`/bin/bash -c 'curl -XGET -k -u "$ELASTIC_USER:$ELASTIC_PASSWORD" "https://localhost:9200/_cluster/health?pretty"'`),
 	)
 	if err != nil {
 		panic(err)
