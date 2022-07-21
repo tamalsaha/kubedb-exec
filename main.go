11a466b6b9c4e82f52597d949f7d16b99cdedc4d
Tamal Saha <tamal@appscode.com>
2022-07-20 19:11:56 -0700

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/apimachinery/pkg/types"
+	"kmodules.xyz/client-go/tools/exec"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/external-dns/source"
 )
 
 func main() {
@@ -18,52 +14,18 @@ func main() {
 	cfg.QPS = 100
 	cfg.Burst = 100
 
-	kc, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		panic(err)
+	pod := types.NamespacedName{
+		Namespace: "demo",
+		Name:      "es-quickstart-0",
 	}
-
-	nodes, err := kc.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-	for _, node := range nodes.Items {
-		fmt.Println(node.GetName())
-	}
-	// os.Exit(1)
-
-	ctx := context.Background()
-	namespace := "default"
-	annotationFilter := ""
-	fqdnTemplate := "xyz.appscode.ninja"
-	combineFqdnAnnotation := false
-	ignoreHostnameAnnotation := false
-	ignoreIngressTLSSpec := true // for nats
-	ignoreIngressRulesSpec := false
-	labelSelector := labels.SelectorFromSet(map[string]string{})
-
-	s1, err := source.NewIngressSource(
-		ctx,
-		kc,
-		namespace,
-		annotationFilter,
-		fqdnTemplate,
-		combineFqdnAnnotation,
-		ignoreHostnameAnnotation,
-		ignoreIngressTLSSpec,
-		ignoreIngressRulesSpec,
-		labelSelector,
+	out, err := exec.Exec(
+		cfg,
+		pod,
+		exec.Container("elasticsearch"),
+		exec.Command("bash", "-c", `curl -XGET -k -u "$ELASTIC_USER:$ELASTIC_PASSWORD" "https://localhost:9200/_cluster/health?pretty"`),
 	)
 	if err != nil {
 		panic(err)
 	}
-	eps, err := s1.Endpoints(ctx)
-	if err != nil {
-		panic(err)
-	}
-	data, err := json.MarshalIndent(eps, "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(data))
+	fmt.Println(out)
 }
